dff80946778b338a69e9e5e9c7ea554b1de30921
Wes Stillwell <wesley.st96@yahoo.de>
2024-06-28 13:34:12 +0200

    firstworking

---
+++ WSyncProApp/Views/SettingsWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WSyncProApp.Views
+{
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Apply_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedTheme = (ThemeComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
+
+            if (selectedTheme == "Light Theme")
+            {
+                ApplyTheme("pack://application:,,,/MaterialDesignThemes;component/Themes/MaterialDesignTheme.Light.xaml");
+            }
+            else if (selectedTheme == "Dark Theme")
+            {
+                ApplyTheme("pack://application:,,,/MaterialDesignThemes;component/Themes/MaterialDesignTheme.Dark.xaml");
+            }
+        }
+
+        private void ApplyTheme(string themeUri)
+        {
+            var resourceDictionary = new ResourceDictionary() { Source = new Uri(themeUri) };
+
+            // Clear existing ResourceDictionary from Application
+            Application.Current.Resources.MergedDictionaries.Clear();
+
+            // Add new ResourceDictionary to Application
+            Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+        }
+    }
+}
