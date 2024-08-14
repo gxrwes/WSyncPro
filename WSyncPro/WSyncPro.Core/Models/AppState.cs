8fe57fee173a70b0f4165a7b31ecd4610d1bdb3b
Wes Stillwell <wesley.st96@yahoo.de>
2024-08-14 12:08:43 +0200

    added archive zipper service and unit test

---
+++ WSyncPro/WSyncPro.Core/Models/AppState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WSyncPro.Core.Models.FileModels;
+
+namespace WSyncPro.Core.Models
+{
+    public class AppState
+    {
+        public List<Job> Jobs { get; set; } //Cached Jobs
+        public List<SyncRun> SyncRuns { get; set; } //Cached SyncRuns
+        public List<WDirectory> WDirectories { get; set; } //Cached Directories
+
+        public List<String> LogLines { get; set; }
+    }
+}
---
+++ WSyncPro/WSyncPro.Core/State/StateManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using WSyncPro.Core.Models;
+
+namespace WSyncPro.Core.State
+{
+    public sealed class StateManager
+    {
+        private static readonly Lazy<StateManager> lazy = new Lazy<StateManager>(() => new StateManager());
+
+        public static StateManager Instance => lazy.Value;
+
+        public List<Job> Jobs { get; private set; }
+        public List<SyncRun> SyncRuns { get; private set; }
+        public string StateFilePath { get; set; } = "AppState.json";
+
+        private StateManager()
+        {
+            LoadState();
+        }
+
+        public void LoadState()
+        {
+            if (File.Exists(StateFilePath))
+            {
+                var json = File.ReadAllText(StateFilePath);
+                var state = JsonConvert.DeserializeObject<AppState>(json);
+
+                Jobs = state.Jobs ?? new List<Job>();
+                SyncRuns = state.SyncRuns ?? new List<SyncRun>();
+            }
+            else
+            {
+                Jobs = new List<Job>();
+                SyncRuns = new List<SyncRun>();
+            }
+        }
+
+        public void SaveState()
+        {
+            var state = new AppState
+            {
+                Jobs = this.Jobs,
+                SyncRuns = this.SyncRuns
+            };
+
+            var json = JsonConvert.SerializeObject(state, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(StateFilePath, json);
+        }
+
+        public void AddJob(Job job)
+        {
+            Jobs.Add(job);
+        }
+
+        public void AddSyncRun(SyncRun syncRun)
+        {
+            SyncRuns.Add(syncRun);
+        }
+
+        public void RemoveJob(Job job)
+        {
+            Jobs.Remove(job);
+        }
+
+        // Other methods to manage state can be added here
+    }
+
+   
+}
