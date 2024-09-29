226cec2dbbaa0fde0c7875c57955223f3955a919
Wes Stillwell <wesley.st96@yahoo.de>
2024-09-29 13:56:33 +0200

    .-

---
+++ WSyncPro/WSyncPro.XPApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WSyncPro.XPApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ WSyncPro/WSyncPro.XPApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WSyncPro.XPApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
