52a05b237729fcf72a505f365a2f9e0e403b0807
Wes Stillwell <wesley.st96@yahoo.de>
2024-11-29 14:41:15 +0100

    .,

---
+++ Wetflix.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Wetflix.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Wetflix.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Wetflix.App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
